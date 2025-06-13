20191d96a830c7a2dc07bd989e5fbb2a896fa056
Guillaume PÉVRIER <guillaumepevrier@gmail.com>
2025-06-13 19:38:54 +0000

    Voici trois images qui constituent la liste de tous les joueurs. Peux-tu

--- src/lib/players.ts
+++ src/lib/players.ts
@@ -1,16 +1,20 @@
+
 export interface Player {
   id: string;
   name: string;
 }
 
 export const players: Player[] = [
-  { id: '1', name: 'Guillaume P.' },
-  { id: '2', name: 'Antoine G.' },
-  { id: '3', name: 'Kylian M.' },
-  { id: '4', name: 'Zinedine Z.' },
-  { id: '5', name: 'Michel P.' },
-  { id: '6', name: 'Lilian T.' },
-  { id: '7', name: 'Didier D.' },
+  { id: '1', name: 'Rom Savatte' },
+  { id: '2', name: 'Vincent Poilvet' },
+  { id: '3', name: 'Drd Julien' },
+  { id: '4', name: 'Germain Mqn' },
+  { id: '5', name: 'Jérémy Aubert' },
+  { id: '6', name: 'Nicolas Beillard' },
+  { id: '7', name: 'Dimitri Hudin' },
+  { id: '8', name: 'Yann Uvy' },
+  { id: '9', name: 'Yoann Poulain' },
+  { id: '10', name: 'Léo Briantais' },
 ];
 
 export type SurveyFormData = {
